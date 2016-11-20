df5735511c2199e7589dc388c15c9deefae539f0
Steven Fleming <sfleming@optoro.com>
2016-11-20 18:04:48 -0500

    starting to sketch out the requests formatter

--- airport_requests_formatter.rb
+++ airport_requests_formatter.rb
@@ -1,7 +1,24 @@
 class AirportRequestsFormatter
+  require 'yaml'
 
   def formatted_requests
+    {
+      request: {
+        passengers: passengers,
+        slice: itinerary
+      }
+    }
+  end
+
+  def passengers
+    {adultCount: 1}
+  end
+
+  def itinerary
 
   end
 
+  def airport_data
+    YAML.load_file('airport_data.yml')
+  end
 end
