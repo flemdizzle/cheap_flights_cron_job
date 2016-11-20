0134767cba7f5b475315989e2b93537f5bc6a0e4
Steven Fleming <sfleming@optoro.com>
2016-11-20 14:50:59 -0500

    first crack at some options

--- integration.rb
+++ integration.rb
@@ -7,6 +7,29 @@ class Integration
   end
 
   def hit_amazon
-    HTTParty.get
+    HTTParty.post(@amz.url, options)
+  end
+
+  def options
+    {
+      query: {
+        key: @amz.api_key,
+      },
+      body: {
+        request: {
+          passengers: {
+            adultCount: 1
+          },
+          slice: [
+            {
+              origin: 'SFO',
+              destination: 'LAX',
+              date: '2014-09-19'
+            }
+          ],
+          solutions: 1
+        }
+      }
+    }
   end
 end
