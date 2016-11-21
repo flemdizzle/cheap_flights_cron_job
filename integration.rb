dbb66960eb0bb9583ed6d566808f110a15b5428f
Steven Fleming <sfleming@optoro.com>
2016-11-20 20:28:59 -0500

    raw json is being store in mongo

--- integration.rb
+++ integration.rb
@@ -2,10 +2,12 @@ class Integration
   load 'qpx_express_credentials.rb'
   load 'airport_requests_formatter.rb'
   require 'httparty'
+  require 'mongo'
 
   def initialize
     @qpx = QpxExpressCredentials.new
     @requests = AirportRequestsFormatter.new.formatted_requests
+    @mongo = initialize_mongo
   end
 
   def run
@@ -33,6 +35,16 @@ class Integration
 
   def process_response(response)
     puts response
+    store_in_mongo(response)
+  end
+
+  def store_in_mongo(response)
+    collection = @mongo[:qpx_json]
+    collection.insert_one(response)
+  end
+
+  def initialize_mongo
+    Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'flight_data')
   end
 
 end
